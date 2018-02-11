0063bdc5f8023b4200f20a723a7174b8c7f75d7d
Bartosz "izdwuut" Konikiewicz <izdwuut@gmail.com>
2018-02-11 11:32:44 +0100

    Bug fixes
    A remove_hidden() function was broken after I merged some arrays.
    No eligible users corner case.
    Refactor get_steam_id() (else clause is not needed).

--- picker.py
+++ picker.py
@@ -22,18 +22,20 @@ def get_steam_id(url):
     response = steamApi.call('ISteamUser.ResolveVanityURL', vanityurl=steamId)['response']
     if response['success'] != 1:
         return steamId
-    else:
-        return str(response['steamid'])
+    return str(response['steamid'])
 
 
 def remove_hidden():
-    response = steamApi.call('ISteamUser.GetPlayerSummaries', steamids=','.join(eligible))['response']['players']
+    ids = []
+    for user, data in eligible.items():
+        ids.append(data['steamId'])
+    response = steamApi.call('ISteamUser.GetPlayerSummaries', steamids=','.join(ids))['response']['players']
     hidden = []
     for player in response:
         if player['communityvisibilitystate'] != 3:
             hidden.append(player['steamid'])
-    for user, steamId in eligible.copy().items():
-        if steamId in hidden:
+    for user, data in eligible.copy().items():
+        if data['steamId'] in hidden:
             del eligible[user]
             violators.append(user)
 
@@ -47,10 +49,9 @@ def get_karma(user):
 
 
 def scrap_comments(submission):
-    # TODO: find_profile_link()
     for comment in submission.comments:
         username = comment.author.name
-        if username.find('_bot') != -1 or username == 'AutoModerator':  # TODO: put in settings.ini
+        if username.find('_bot') != -1 or username == 'AutoModerator':
             continue
         for a in Soup(comment.body_html, 'html.parser')('a'):
             url = a.get('href')
@@ -66,22 +67,21 @@ if __name__ == '__main__':
     for user in eligible.copy():
         eligible[user]['steamId'] = pool.apply_async(get_steam_id, [eligible[user]['url']])
         eligible[user]['karma'] = pool.apply_async(get_karma, [user])
-    for user, result in eligible.items():
-        eligible[user]['steamId'] = result['steamId'].get()
+    for user, data in eligible.items():
+        eligible[user]['steamId'] = data['steamId'].get()
     remove_hidden()
     for user in eligible.copy():
         # TODO: handle HTTP 500 error
         eligible[user]['level'] = pool.apply_async(get_steam_level, [eligible[user]['steamId']])
     for user in eligible.copy():
-        level = eligible[user]['level'].get()
-        karma = eligible[user]['karma'].get()
-        if level < minLevel or karma < minKarma:
+        if eligible[user]['level'].get() < minLevel or eligible[user]['karma'].get() < minKarma:
             eligible.pop(user)
             violators.append(user)
 
-    print('Users that violate rules: ' + ', '.join(violators))
-    print('Users eligible for drawing: ' + ', '.join(eligible.keys()))
-    # TODO: a list can be empty
-    print('Winner: ' + random.choice(list(eligible)))
-    # TODO: handle exceptions
-    print(eligible)
+    if violators:
+        print('Users that violate rules: ' + ', '.join(violators) + '.')
+    if eligible:
+        print('Users eligible for drawing: ' + ', '.join(eligible.keys()) + '.')
+        print('Winner: ' + random.choice(list(eligible)))
+    else:
+        print('No eligible users.')
