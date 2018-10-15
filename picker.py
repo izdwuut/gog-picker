25944a85af3be68b751e8fe0b1c79dacd0fd8715
Bartosz " izdwuut <izdwuut@gmail.com>
2018-10-15 14:13:43 +0200

    Add drawing multiple items to Random class.

--- picker.py
+++ picker.py
@@ -103,13 +103,26 @@ class Reddit:
 
 class Random:
     def item(self, items):
-        max = len(items) - 1
+        max = self._get_max(items)
         pos = self._get_integer(max)
         return items[pos]
 
+    def items(self, items, n, replacement=False):
+        max = self._get_max(items)
+        integers = self._get_integers(n, max, replacement=replacement)
+        set = [items[i] for i in integers]
+        return set
+
     def _get_integer(self, max=0, min=0):
-        integers = self.api.generate_integers(1, min, max)
+        integers = self._get_integers(min=min, max=max)
         return integers[0]
+
+    def _get_integers(self, n=1, max=0, min=0, replacement=False):
+        integers = self.api.generate_integers(n, min, max, replacement=replacement)
+        return integers
+
+    def _get_max(self, items):
+        return len(items) - 1
 
     def __init__(self, settings):
         self.api = RandomOrgClient(settings['api_key'])
