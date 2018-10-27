d0b2fe8f538e605c83588bd6562f635eed9a0354
Bartosz " izdwuut <izdwuut@gmail.com>
2018-10-27 14:15:23 +0200

    Fix: cover more cases in Steam.get_id().
    The script would previously fail for this class of Steam URLs:
    https://steamcommunity.com/profiles/any_non_numeric_string

--- _steam.py
+++ _steam.py
@@ -16,14 +16,16 @@ class Steam:
         games = self.get_user_games(steamid)
         return bool(games)
 
-    # TODO: handle /profiles/{non-numeric}
-    # TODO: throw an exception if an url is invalid.
-    # TODO: fix invalid url with /profiles
     def get_id(self, url):
         url = url.strip('/')
         path = urlparse(url).path.strip('/').split('/')
         if path[0] == 'profiles':
-            return path[1]
+            try:
+                id = path[1]
+                if isinstance(int(id), int):
+                    return id
+            except ValueError:
+                return None
         response = self.resolve_vanity_url(path[1])
         if response['success'] == 1:
             return response['steamid']
