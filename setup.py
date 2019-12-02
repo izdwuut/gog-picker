73c346ab85f544f67e5f94f513d9704e6dea33aa
Bartosz " izdwuut <izdwuut@gmail.com>
2019-12-02 20:14:17 +0100

    Bump version to 0.5.0.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gog-picker",
-    version="0.4.1",
+    version="0.5.0",
     author='Bartosz "izdwuut" Konikiewicz',
     author_email="izdwuut@gmail.com",
     description="Gift of Games Picker for Reddit",
@@ -13,19 +13,19 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/izdwuut/gog-picker",
     packages=setuptools.find_packages(),
-	include_package_data=True,
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Microsoft :: Windows",
-		"Natural Language :: English"
+        "Natural Language :: English"
     ],
-	install_requires=[
-		"steam",
-		"praw",
-		"prawcore",
-		"beautifulsoup4",
-		"rdoclient-py3",
-		"requests"
-	]
+    install_requires=[
+        "steam",
+        "praw",
+        "prawcore",
+        "beautifulsoup4",
+        "rdoclient-py3",
+        "requests"
+    ]
 )
