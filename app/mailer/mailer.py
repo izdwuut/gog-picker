7fb64b06e3e2696d437b6eea431d678dfe3e1fcb
Bartosz " izdwuut <izdwuut@gmail.com>
2020-02-05 10:57:02 +0100

    Handle JSON request in /mailer/send endpoint.

--- app/mailer/mailer.py
+++ app/mailer/mailer.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, current_app, jsonify
 from flask_jwt_extended import jwt_required
 from app.reddit import Reddit
+from app._errors import Errors
 
 mailer = Blueprint('mailer', __name__, url_prefix='/mailer')
 
@@ -8,14 +9,18 @@ mailer = Blueprint('mailer', __name__, url_prefix='/mailer')
 @mailer.route('/send', methods=['POST'])
 @jwt_required
 def send_message():
-    if 'username' not in request.form:
-        return {'error': 'No required parameter: username.'}
-    if 'subject' not in request.form:
-        return {'error': 'No required parameter: subject.'}
-    if 'body' not in request.form:
-        return {'error': 'No required parameter: body.'}
-
+    if not request.is_json:
+        return jsonify({"error": Errors.MISSING_JSON}), 400
+    username = request.json.get('username', None)
+    subject = request.json.get('subject', None)
+    body = request.json.get('body', None)
+    if not username:
+        return jsonify({"error": Errors.NO_REQUIRED_FIELD + 'username.'}), 400
+    if not subject:
+        return jsonify({"error": Errors.NO_REQUIRED_FIELD + 'subject.'}), 400
+    if not body:
+        return jsonify({"error": Errors.NO_REQUIRED_FIELD + 'body.'}), 400
     reddit = Reddit(None, current_app.config['REDDIT'])
-    reddit.send_message(request.form['username'], request.form['subject'], request.form['body'])
+    reddit.send_message(username, subject, body)
 
     return jsonify({'success': 'Sent a message to {}.'.format(request.form['username'])}), 200
