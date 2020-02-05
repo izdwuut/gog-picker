dd0c7c905a992d158d864488c8eacbc77224cf82
Bartosz " izdwuut <izdwuut@gmail.com>
2020-02-05 18:45:07 +0100

    Add frontend.

---
+++ frontend/src/app/services/rest.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestService {
+
+  constructor(private http: HttpClientgit ) { }
+}
