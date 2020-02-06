f5906b38f480a78b48aedcccc76b22085b183a9f
Bartosz " izdwuut <izdwuut@gmail.com>
2020-02-06 11:39:51 +0100

    Add input field to HomeComponent.

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../services/rest.service'
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   thread = ''
+  inputHint = ''
+  hasErrors = false
   
-  constructor() { }
+  constructor(private rest: RestService) { }
 
   ngOnInit() {
+  }
+
+  onSearchChange(): void {  
+    this.rest.isUrlValid(this.thread).subscribe(data => {
+      this.hasErrors = false
+      this.inputHint = data['success']
+    },
+    error => {
+      this.hasErrors = true
+      if(this.thread === '') {
+        this.inputHint = ''
+      } else {
+        this.inputHint = error['error']['error']
+      }
+    })
   }
 
 }
