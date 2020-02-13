61dc2272be8a72c88bb5127dd8076a65c1fe60c2
Bartosz " izdwuut <izdwuut@gmail.com>
2020-02-13 11:20:28 +0100

    Add unsubscribe to ResultsComponent.

--- frontend/src/app/results/results.component.ts
+++ frontend/src/app/results/results.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RestService } from '../services/rest.service';
 import { ActivatedRoute } from '@angular/router';
 import { Results } from '../models/results.model';
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
-export class ResultsComponent implements OnInit {
+export class ResultsComponent implements OnInit, OnDestroy {
   hash: String
   results: Results
+  resultsSubscription: Subscription
 
   constructor(private rest: RestService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.hash = this.route.snapshot.paramMap.get('hash')
-    this.rest.getResults(this.hash).subscribe(results => {
-      console.log(results)
+    this.resultsSubscription = this.rest.getResults(this.hash).subscribe(results => {
       this.results = results
     })
   }
@@ -32,5 +33,9 @@ export class ResultsComponent implements OnInit {
       links.push(link)
     })
     return links.join(', ')
+  }
+
+  ngOnDestroy() {
+    this.resultsSubscription.unsubscribe()
   }
 }
