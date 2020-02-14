b55efbf6cb66abb829710587e9c6e0cb10dc66fd
Bartosz " izdwuut <izdwuut@gmail.com>
2020-02-14 12:36:34 +0100

    Bump version number to 0.9.3.

--- backend/config.py
+++ backend/config.py
@@ -9,7 +9,7 @@ class RedditConfig:
     CLIENT_SECRET = os.environ['GOG_PICKER_REDDIT_CLIENT_SECRET']
     USERNAME = os.environ['GOG_PICKER_REDDIT_USERNAME']
     PASSWORD = os.environ['GOG_PICKER_REDDIT_PASSWORD']
-    USER_AGENT = 'python:gog-picker:v0.9.2 (by /u/izdwuut)'
+    USER_AGENT = 'python:gog-picker:v0.9.3 (by /u/izdwuut)'
     NOT_ENTERING = 'not entering'
     MIN_KARMA = 300
 
