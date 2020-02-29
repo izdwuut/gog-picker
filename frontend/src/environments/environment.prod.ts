47090587a13f4eb456ba06261e9beccd938d4ff2
Bartosz " izdwuut <izdwuut@gmail.com>
2020-02-29 12:26:03 +0100

    Add a domain.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://159.203.105.68:5000/",
+  apiUrl: "http://gogpicker.com:5000/",
   minKarma: 300,
   hoarderNumber: 500,
   minLevel: 2,
